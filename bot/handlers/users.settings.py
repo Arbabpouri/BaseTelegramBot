55ee3c80723b5d41ac74ddd48483343984dbafb3
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-03-30 19:00:48 +0330

    make functions for handlers

---
+++ bot/handlers/users.settings.py
@@ -0,0 +1,17 @@
+from telethon.events import NewMessage
+from telethon.custom import Message
+from . import client
+
+
+
+async def ban_user(event: Message) -> None:
+    pass
+
+
+async def unban_user(event: Message) -> None:
+    pass
+
+
+async def show_user_stats(event: Message) -> None:
+    pass
+
