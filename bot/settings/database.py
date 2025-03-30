a458ab1bd891579989b97cd81335c6df3f5cc1c4
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-03-30 21:30:14 +0330

    add get_session function

--- bot/settings/database.py
+++ bot/settings/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
 from sqlalchemy import create_engine
-
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
@@ -15,14 +14,6 @@ class Base(DeclarativeBase):
     pass
 
 
-# def default_data() -> None:
-
-#     with Session(bind=engine) as session:
-
-#         for admin in BotConfig.DEFULT_ADMINS_USER_ID:
-#             session.add(User(user_id=admin, is_admin=True))
-
-#         configs = Configs()
-
-#         session.add_all([configs, ])
-#         session.commit()
+def get_session() -> Session:
+    with Session(engine) as session:
+        yield session
