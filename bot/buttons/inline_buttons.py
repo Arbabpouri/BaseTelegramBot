7fc52d9abb621c335a089d5d979e58a691bd9b86
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-03-31 17:25:31 +0330

    update inline buttons

--- bot/buttons/inline_buttons.py
+++ bot/buttons/inline_buttons.py
@@ -1,6 +1,7 @@
 from telethon import Button
-from typing import Optional
+from enum import IntEnum
 from functions.database_functions import get_channels
+
 
 # All Inline Button Data
 class InlineButtonsData:
@@ -61,80 +62,15 @@ class InlineButtonString:
     BACK = "📍 | بازگشت"
 
 
+# all backs enum, using for InlineButtons.back_to
+class BackToEnum(IntEnum):
+    ADMIN_PANEL = 0
+    
+
 # All Inline Button
 class InlineButtons:
-
-    BACK_TO_ADMIN = (
-        Button.inline(text=InlineButtonString.BACK, data=InlineButtonsData.BACK_TO_ADMIN),
-    )
-
-    ADMIN_PANEL = (
-        (
-            Button.inline(text=InlineButtonString.BOT_STATS, data=InlineButtonsData.BOT_STATS),
-        ),
-        (
-            Button.inline(text=InlineButtonString.ADMIN_SETTING_PANEL, data=InlineButtonsData.ADMIN_SETTING_PANEL),
-            Button.inline(text=InlineButtonString.USER_SETTING_PANEL, data=InlineButtonsData.USER_SETTING_PANEL)
-        ),
-        (
-            Button.inline(text=InlineButtonString.SEND_PANEL, data=InlineButtonsData.SEND_PANEL),
-            Button.inline(text=InlineButtonString.CHANNEL_PANEL, data=InlineButtonsData.CHANNEL_PANEL)
-        ),
-        (
-            Button.inline(text=InlineButtonString.CHANGE_CONFIGS, data=InlineButtonsData.CHANGE_CONFIGS),
-        ),
-    )
-
-    ADMIN_SETTING = (
-        (
-            Button.inline(text=InlineButtonString.ADD_ADMIN, data=InlineButtonsData.ADD_ADMIN),
-            Button.inline(text=InlineButtonString.DELETE_ADMIN, data=InlineButtonsData.DELETE_ADMIN)
-        ),
-        (
-            Button.inline(text=InlineButtonString.SHOW_ADMINS, data=InlineButtonsData.SHOW_ADMINS),
-        ),
-        BACK_TO_ADMIN
-    )
-
-    USER_SETTING = (
-        (
-            Button.inline(text=InlineButtonString.BAN_USER, data=InlineButtonsData.BAN_USER),
-            Button.inline(text=InlineButtonString.UNBAN_USER, data=InlineButtonsData.UNBAN_USER),
-        ),
-        (
-            Button.inline(text=InlineButtonString.SHOW_USER_INFO, data=InlineButtonsData.SHOW_USER_INFO),
-        ),
-        BACK_TO_ADMIN
-    )
-
-    SEND_PANEL = (
-        (
-            Button.inline(text=InlineButtonString.SEND_TO_USER, data=InlineButtonsData.SEND_TO_USER),
-            Button.inline(text=InlineButtonString.SEND_TO_USERS, data=InlineButtonsData.SEND_TO_USERS)
-        ),
-        BACK_TO_ADMIN
-    )
-
-    CONFIGS_PANEL = (
-        (
-            Button.inline(text=InlineButtonString.CHANGE_RULES_TEXT, data=InlineButtonsData.CHANGE_RULES_TEXT),
-        ),
-        (
-            Button.inline(text=InlineButtonString.CHANGE_HELP_TEXT, data=InlineButtonsData.CHANGE_HELP_TEXT),
-        ),
-        (
-            Button.inline(text=InlineButtonString.CHANGE_ENTERY_PRIZE, data=InlineButtonsData.CHANGE_ENTERY_PRIZE),
-        ),
-        (
-            Button.inline(text=InlineButtonString.CHANGE_REFERRAL_BONUS, data=InlineButtonsData.CHANGE_REFERRAL_BONUS),
-        ),
-        (
-            Button.inline(text=InlineButtonString.CHANGE_TRUST_CHANNEL, data=InlineButtonsData.CHANGE_TRUST_CHANNEL),
-        ),
-        BACK_TO_ADMIN
-    )
-
-
+    
+    
     @staticmethod
     async def channels_panel():
         
@@ -163,14 +99,99 @@ class InlineButtons:
     @staticmethod
     def check_joined(user_id: int | None = None):
         return Button.inline(text=InlineButtonString.JOINED_IN_CHANNEL, data=InlineButtonsData.joined_in_channel(user_id))
-
-
+    
+    
     @staticmethod
-    def back_to(
-        admin_panel: Optional[bool] = False,
-    ):
+    def back_to(back_to: BackToEnum):
         
-        text = InlineButtonString.BACK
-        data = InlineButtonsData.BACK_TO_ADMIN
+        match (back_to):
+            
+            case BackToEnum.ADMIN_PANEL:
+                text = InlineButtonString.BACK
+                data = InlineButtonsData.BACK_TO_ADMIN
+            
+            case _:
+                text = InlineButtonString.BACK
+                data = None
+                
         
         return Button.inline(text=text, data=data)
+
+
+    ADMIN_PANEL = (
+        (
+            Button.inline(text=InlineButtonString.BOT_STATS, data=InlineButtonsData.BOT_STATS),
+        ),
+        (
+            Button.inline(text=InlineButtonString.ADMIN_SETTING_PANEL, data=InlineButtonsData.ADMIN_SETTING_PANEL),
+            Button.inline(text=InlineButtonString.USER_SETTING_PANEL, data=InlineButtonsData.USER_SETTING_PANEL)
+        ),
+        (
+            Button.inline(text=InlineButtonString.SEND_PANEL, data=InlineButtonsData.SEND_PANEL),
+            Button.inline(text=InlineButtonString.CHANNEL_PANEL, data=InlineButtonsData.CHANNEL_PANEL)
+        ),
+        (
+            Button.inline(text=InlineButtonString.CHANGE_CONFIGS, data=InlineButtonsData.CHANGE_CONFIGS),
+        ),
+    )
+
+
+    ADMIN_SETTING = (
+        (
+            Button.inline(text=InlineButtonString.ADD_ADMIN, data=InlineButtonsData.ADD_ADMIN),
+            Button.inline(text=InlineButtonString.DELETE_ADMIN, data=InlineButtonsData.DELETE_ADMIN)
+        ),
+        (
+            Button.inline(text=InlineButtonString.SHOW_ADMINS, data=InlineButtonsData.SHOW_ADMINS),
+        ),
+        (
+            back_to(back_to=BackToEnum.ADMIN_PANEL),
+        )
+    )
+
+
+    USER_SETTING = (
+        (
+            Button.inline(text=InlineButtonString.BAN_USER, data=InlineButtonsData.BAN_USER),
+            Button.inline(text=InlineButtonString.UNBAN_USER, data=InlineButtonsData.UNBAN_USER),
+        ),
+        (
+            Button.inline(text=InlineButtonString.SHOW_USER_INFO, data=InlineButtonsData.SHOW_USER_INFO),
+        ),
+        (
+            back_to(back_to=BackToEnum.ADMIN_PANEL),
+        )
+    )
+
+
+    SEND_PANEL = (
+        (
+            Button.inline(text=InlineButtonString.SEND_TO_USER, data=InlineButtonsData.SEND_TO_USER),
+            Button.inline(text=InlineButtonString.SEND_TO_USERS, data=InlineButtonsData.SEND_TO_USERS)
+        ),
+        (
+            back_to(back_to=BackToEnum.ADMIN_PANEL),
+        )
+    )
+
+
+    CONFIGS_PANEL = (
+        (
+            Button.inline(text=InlineButtonString.CHANGE_RULES_TEXT, data=InlineButtonsData.CHANGE_RULES_TEXT),
+        ),
+        (
+            Button.inline(text=InlineButtonString.CHANGE_HELP_TEXT, data=InlineButtonsData.CHANGE_HELP_TEXT),
+        ),
+        (
+            Button.inline(text=InlineButtonString.CHANGE_ENTERY_PRIZE, data=InlineButtonsData.CHANGE_ENTERY_PRIZE),
+        ),
+        (
+            Button.inline(text=InlineButtonString.CHANGE_REFERRAL_BONUS, data=InlineButtonsData.CHANGE_REFERRAL_BONUS),
+        ),
+        (
+            Button.inline(text=InlineButtonString.CHANGE_TRUST_CHANNEL, data=InlineButtonsData.CHANGE_TRUST_CHANNEL),
+        ),
+        (
+            back_to(back_to=BackToEnum.ADMIN_PANEL),
+        )
+    )
