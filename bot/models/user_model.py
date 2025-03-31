6bf86ed1155555f99fc9ee51c920d4d7984da6c0
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-03-31 17:33:25 +0330

    update user model

--- bot/models/user_model.py
+++ bot/models/user_model.py
@@ -12,5 +12,5 @@ class UserModel(Base):
     is_admin: Mapped[bool] = mapped_column(default=False)
     is_ban: Mapped[bool] = mapped_column(default=False)
     referral_active: Mapped[bool] = mapped_column(nullable=True, default=None)
-    invited_by: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=True, default=None)
-    referrals: Mapped[List["UserModel"]] = relationship('User', remote_side='User.id', backref=backref('user_referrals'))
+    invited_by: Mapped[int] = mapped_column(Integer, ForeignKey("users.user_id"), nullable=True, default=None)
+    referrals: Mapped[List["UserModel"]] = relationship('User', remote_side='User.user_id', backref=backref('user_referrals'))
