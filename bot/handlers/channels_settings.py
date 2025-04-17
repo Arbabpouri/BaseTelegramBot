7128e8425e89787934b2a47f418d3f6ff7831836
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-04-17 16:14:26 +0330

    end

--- bot/handlers/channels_settings.py
+++ bot/handlers/channels_settings.py
@@ -1,11 +1,26 @@
+# reion imports
+
 from telethon.events import NewMessage, CallbackQuery
 from telethon.custom import Message
-from functions.database_functions import add_channel, remove_channel, get_channels
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.types import Channel, PeerChannel
+from functions.database_functions import add_channel, remove_channel, get_channels, get_channel
 from functions.step_functions import Parts, set_step, delete_step
 from buttons.inline_buttons import InlineButtons, InlineButtonsData, BackToEnum
-from settings.strings import ENTER_USER_ID, SELECT, ADD_CHANNEL, ERROR, DELETED
+from settings.strings import (
+    SELECT, 
+    ADD_CHANNEL, 
+    ERROR, 
+    DELETED,
+    ADDED,
+    CHANNEL_ALREADY_EXIST,
+    BOT_NOT_ADMIN
+)
 from . import client
 
+# endregion
+
+# region CallBackQuery Handlers
 
 # CallbackQuery handler, show bots channels settings
 @client.on(event=CallbackQuery(data=InlineButtonsData.CHANNEL_PANEL))
@@ -48,7 +63,54 @@ async def delete_channel(event: Message) -> None:
         pass
 
 
+# endregion
+
+
+# region NewMessage Handlers
+
+
 # NewMessage handler, get channels user id and add to db
-@client.on(event=NewMessage())
+@client.on(event=NewMessage(forwards=True, incoming=True))
 async def new_channel(event: Message) -> None:
-    pass
+    
+    try:
+        
+        channel = await event.forward.get_chat()
+        if not isinstance(channel , Channel):
+            await event.reply(ADD_CHANNEL)
+            return
+            
+        if not channel.admin_rights:
+            await event.reply(BOT_NOT_ADMIN, buttons=InlineButtons.CANCEL_ADMIN)
+            return
+        
+        check_channel = get_channel(channel_id=channel.id)
+
+        if not check_channel:
+            channel_info = await client(GetFullChannelRequest(PeerChannel(int(check_channel.id))))
+            add = add_channel(
+                channel_id=channel.id,
+                channel_name=channel.title,
+                channel_url=channel_info.full_chat.exported_invite.link
+            )
+            
+            if not add:
+                await event.reply(ERROR, buttons=await InlineButtons.channels_panel())
+                delete_step(user_id=event.sender_id)
+            
+            else:
+                await event.reply(ADDED, buttons=await InlineButtons.channels_panel())
+                delete_step(user_id=event.sender_id)
+    
+        else:
+            await event.reply(CHANNEL_ALREADY_EXIST)
+    
+    except Exception as e:
+        await event.reply(f"{ADD_CHANNEL}\n\n{ERROR} : \n{e}", buttons=InlineButtons.CANCEL_ADMIN)
+                        
+    finally:
+        
+        pass
+
+
+# endregion
