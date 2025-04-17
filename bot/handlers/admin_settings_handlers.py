70a984e17269df506fa5f0683433643169630e8e
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-04-17 15:41:57 +0330

    end admin

--- bot/handlers/admin_settings_handlers.py
+++ bot/handlers/admin_settings_handlers.py
@@ -1,12 +1,23 @@
+# region imports
+
 from telethon.events import NewMessage, CallbackQuery
 from telethon.custom import Message
-from functions.database_functions import add_admin, remove_admin, user_is_admin, get_admins
+from functions.database_functions import add_admin, remove_admin, user_is_admin, get_admins, get_user
 from functions.step_functions import Parts, set_step, delete_step
 from buttons.inline_buttons import InlineButtons, InlineButtonsData, BackToEnum
 from buttons.commands import Commands
 from buttons.text_buttons import TextButtonsString, TextButtons
-from settings.strings import show_admins as string_show_admins, ENTER_USER_ID, SELECT, ADMIN_PANEL
+from settings.strings import (
+    show_admins as string_show_admins, 
+    ENTER_USER_ID, 
+    SELECT, 
+    ADMIN_PANEL,
+    USER_NOT_EXIST,
+    ADDED
+)
 from . import client
+
+# endregion
 
 
 # region CallBackQuery Handlers
@@ -42,7 +53,7 @@ async def add_admin_set_step(event: CallbackQuery.Event) -> None:
     try:
     
         set_step(user_id=event.sender_id, step=Parts.ADD_ADMIN)
-        await event.edit(ENTER_USER_ID, buttons=InlineButtons.CANCEL)
+        await event.edit(ENTER_USER_ID, buttons=InlineButtons.CANCEL_ADMIN)
     
     finally:
         pass
@@ -55,7 +66,7 @@ async def remove_admin_set_step(event: CallbackQuery.Event) -> None:
     try:
     
         set_step(user_id=event.sender_id, step=Parts.DELETE_ADMIN)
-        await event.edit(ENTER_USER_ID, buttons=InlineButtons.CANCEL)
+        await event.edit(ENTER_USER_ID, buttons=InlineButtons.CANCEL_ADMIN)
         
     finally:
         pass
@@ -65,6 +76,7 @@ async def remove_admin_set_step(event: CallbackQuery.Event) -> None:
 
 
 # region NewMessage Handlers
+
 
 # NewMessage handler, open panel admin
 @client.on(event=NewMessage(incoming=True, pattern=Commands.ADMIN))
@@ -78,15 +90,37 @@ async def open_admin_panel(event: Message) -> None:
 
 
 # NewMessage handler, get admin user id and add to db
-@client.on(event=NewMessage(incoming=True))
+@client.on(event=NewMessage(incoming=True, pattern="[0-9]*"))
 async def new_admin(event: Message) -> None:
-    pass
+    
+    try:
+        
+        user = int(event.message.message)
+        add = add_admin(user_id=user)
+        if not add:
+            await event.reply(USER_NOT_EXIST, buttons=InlineButtons.CANCEL_ADMIN)
+            return None
+        
+        await event.reply(ADDED, buttons=InlineButtons.ADMIN_SETTING)
+    
+    finally:
+        pass
 
 
 # NewMessage handler, get admin user id and check in db? and remove from db
 @client.on(event=NewMessage(incoming=True))
-async def remove_admin(event: Message) -> None:
-    pass
+async def delete_admin(event: Message) -> None:
+    try:
+        user = int(event.message.message)
+        add = remove_admin(user_id=user)
+        if not add:
+            await event.reply(USER_NOT_EXIST, buttons=InlineButtons.CANCEL_ADMIN)
+            return None
+        
+        await event.reply(ADDED, buttons=InlineButtons.ADMIN_SETTING)
+    
+    finally:
+        pass
 
 
 # endregion
