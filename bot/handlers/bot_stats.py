f8f3226e26972ff642f22bebd9523c67e1786743
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-04-20 16:08:57 +0330

    set filters in bot stats handlers

--- bot/handlers/bot_stats.py
+++ bot/handlers/bot_stats.py
@@ -5,6 +5,7 @@ from buttons.inline_buttons import InlineButtons, InlineButtonsData, BackToEnum
 from buttons.text_buttons import TextButtons, TextButtonsString
 from settings.strings import bot_stats, my_account
 from functions.database_functions import get_users, get_channels, get_user
+from functions.filters_functions import filter_user_move, filter_admin_move
 from . import client
 
 # endregion
@@ -13,7 +14,7 @@ from . import client
 # region CallbackQuery Handlers
 
 # CallbackQuery handler, show bot stats
-@client.on(event=CallbackQuery(data=InlineButtonsData.BOT_STATS))
+@client.on(event=CallbackQuery(data=InlineButtonsData.BOT_STATS, func=filter_admin_move))
 async def bot_status(event: CallbackQuery.Event) -> None:
     
     try:
@@ -30,7 +31,7 @@ async def bot_status(event: CallbackQuery.Event) -> None:
 # region NewMessage Handlers
 
 # NewMessage handler, show user stats
-@client.on(event=NewMessage(pattern=TextButtonsString.MY_ACCOUNT))
+@client.on(event=NewMessage(pattern=TextButtonsString.MY_ACCOUNT, func=filter_user_move))
 async def user_account_info(event: CallbackQuery.Event) -> None:
     
     try:
